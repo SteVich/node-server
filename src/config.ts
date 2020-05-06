611fb73be9cb8c21c542d5991d3b90c683f063de
SteVi <trozzato@gmail.com>
2020-05-06 18:27:08 +0300

    added db credentials

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,11 @@ import {Specialty} from "./model/specialty";
 
 const typeOrmConfig: PostgresConnectionOptions = {
     type: "postgres",
-    host: "localhost",
+    host: "ec2-54-246-87-132.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    username: "postgres",
-    password: "1234",
-    database: "angular",
+    username: "zrhncsonbavazm",
+    password: "b2d2b385d02e83bf524e2a5caf62a0d22555aaa0e60e84fd5f925e1f1244df78",
+    database: "dapfabilptiap1",
     synchronize: true,
     logging: false,
     entities: [
