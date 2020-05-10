e46a30ef51c627f0f3a08c0df70f1d5dfe40cd40
SteVi <trozzato@gmail.com>
2020-05-10 13:05:35 +0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,13 @@ const app = express()
     .use(groupRouter)
     .use(adminRouter);
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://univer-sv.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, function() {
