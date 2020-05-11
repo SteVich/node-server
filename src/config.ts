9ccdda1dfe3b785a3799a0cea06c4fc77fb0ed79
SteVi <trozzato@gmail.com>
2020-05-11 11:37:34 +0300

    updated db credentials

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ const typeOrmConfig: PostgresConnectionOptions = {
     type: "postgres",
     host: "ec2-46-137-84-140.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    username: "axblpefsjqxjoo",
-    password: "56bd0b2011943a3640f32d29d680c093b65a4b685596113da259c45d97f7f2cc",
-    database: "d655ptdtso1nj0",
+    username: "qwkblrnhbpsvwj",
+    password: "27e5dc0781d4f44c30084b2c4c0aeb1b3934d701945c957c769445f450e65c94",
+    database: "da8ric32f5pm4m",
     synchronize: false,
     logging: false,
     entities: [
