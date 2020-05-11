70f328464c93c488834013dc93d2f7e507f271a2
SteVi <trozzato@gmail.com>
2020-05-11 12:52:47 +0300

    updated db password

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ const typeOrmConfig: PostgresConnectionOptions = {
     host: "ec2-34-200-72-77.compute-1.amazonaws.com",
     port: 5432,
     username: "wofqajshrfbwpg",
-    password: "eefec3d5b967d0225dad438c85c0cf878960e6de0643671250f4d1305c0c5c2b",
+    password: "5c54d7f17fee8696243bc715707705a84bdcef106b9c06820d9a0f0c6194556c",
     database: "d8t7u84tfhjv8n",
     synchronize: true,
     logging: false,
